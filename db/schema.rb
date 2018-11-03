a04ed583a7f75d1c6cff5914b1a0a6efa4b07103
VINCENT KUSA <vincentkusa170203@daystar.ac.ke>
2018-11-03 05:30:25 +0300

    user validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_02_070749) do
+ActiveRecord::Schema.define(version: 2018_11_02_101646) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -22,6 +23,7 @@ ActiveRecord::Schema.define(version: 2018_11_02_070749) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
